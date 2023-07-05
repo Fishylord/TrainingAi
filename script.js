ce646fb9cf35f60f4b25d03bf4e247f0e4b4a19b
Fishylord <chunyee04@outlook.com>
2023-07-05 14:20:36 +0800

    A

--- script.js
+++ script.js
@@ -5,30 +5,56 @@ import { PromptTemplate } from "langchain/prompts";
 import { LLMChain } from "langchain/chains";
 import { createConnection } from "mysql2/promise";
 import { JSONLoader } from "langchain/document_loaders/fs/json";
+import * as fs from "fs";
+import { JsonSpec } from "langchain/tools";
 config()
 
-const chat = new PromptLayerChatOpenAI({
-    returnPromptLayerId: true,
-    openAIApiKey: process.env.OPENAI_API_KEY,
-    temperature: 0.9,
-    modelName: "gpt-3.5-turbo-0613",
-});
 
-const template = "What is a good name for a company that makes {product}?";
-const prompt = new PromptTemplate({
-  template: template,
-  inputVariables: ["product"],
-});
+const run = async () => {
+    // Load the JSON data
+    let Jdata;
+    try {
+        const jsonData = await fs.promises.readFile("SampleData.JSON", "utf8");
+        Jdata = JSON.parse(jsonData);
+      } catch (e) {
+        console.error(e);
+        return;
+      }
 
-const loader = new JSONLoader("src/document_loaders/example_data/SampleData.json");
-console.log(prompt);
-const respA = await chat.generate([
-    [
-    new SystemChatMessage(
-      
-    ),
-    ],
-]);
+    const chat = new PromptLayerChatOpenAI({
+        openAIApiKey: process.env.OPENAI_API_KEY,
+        temperature: 0.9,
+        modelName: "gpt-3.5-turbo-0613",
+    });
+    
+    const template = `What insights can you provide about the data? 
+    You are a "Data Analysis" working as a assistant providing insights for your bosses and collogues for the daily workflow.
+    Provide Information like deadline, issues and updates on data if possible depending on the prompt. 
+    {{data}} 
+    `;
+    const prompt = new PromptTemplate({
+    template: template,
+    inputVariables: ["data"],
+    });
 
-console.log(respA);
-console.log(JSON.stringify(respA, null, 3));
+    
+    console.log(Jdata);
+    // console.log(formattedPrompt);
+    const chain = new LLMChain({ llm: chat, prompt: prompt });
+    console.log(chain)
+    const response = await chain.call({ data: "say 123 if you see this" });
+
+    console.log(response);
+    // const respA = await chat.generate([
+    //     [
+    //     new SystemChatMessage(
+    //         prompt
+    //     ),
+    //     ],
+    // ]);
+
+    // console.log(respA);
+    // console.log(JSON.stringify(respA, null, 3));
+};
+
+run();
