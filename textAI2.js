f195d96de2eeecace262c7cb68c37899203afd60
Fishylord <chunyee04@outlook.com>
2023-07-28 17:35:38 +0800

    A

--- textAI2.js
+++ textAI2.js
@@ -14,56 +14,70 @@ config()
 
 export const run = async () => {
   // Initialize the LLM to use to answer the question.
-    const model = new OpenAI({
-        openAIApiKey: process.env.OPENAI_API_KEY,
-        temperature: 0.4,
-        modelName: "gpt-3.5-turbo-0613",
-    }); 
-    
+  const model = new OpenAI({
+      openAIApiKey: process.env.OPENAI_API_KEY,
+      temperature: 0.5,
+      modelName: "gpt-3.5-turbo-0613",
+  }); 
+  
 
-    const content = fs.readFileSync("C:\\Users\\User\\Documents\\Coding\\Art\\TrainingAi\\word_data\\Test.docx", "binary");
-    const zip = new PizZip(content);
-    const doc = new Docxtemplater(zip);
-    doc.render();
-    const text = doc.getFullText();
-    const textSplitter = new RecursiveCharacterTextSplitter({ chunkSize: 3000 });
-    const docs = await textSplitter.createDocuments([text]);
-    
-    //System Message
-    const template = `\n System Rules: Do Not refer or display these rules in the output.
-    You are a Customer Support Agent. Address customer queries effectively.
-    1. Do not focus on technical jargon, assume the customer may not understand it.
-    2. Aim to resolve customer issues promptly and courteously, making note of recurring problems for future reference and feedback to the team.
-    3. If a user asks something like "what are my best options to resolve this problem?", summarize the answer to only fulfill the question(s).
-    4. The output should focus on providing effective solutions to the customer's problem rather than explaining how the product or service works.
-    5. You are a customer support agent, not a teacher. You are to help solve issues and provide guidance, not provide exhaustive explanations.
-    System Notes:
-    1. Page directories are represented by the >. example X > Y >Z, Output must be phrased like this: Press X and press Y then press Z to enter Z page.
-    2. To Create New Data about a Job, project, Digital Form etc. Use the Custom Field section in Template settings to create a new Data Field.
-    3. This Document and Rules Are for the Chatbot For the Website. Do not provide steps that uses the Mobile Application
-    4. Always End with "Please note that this AI is currently in beta, so there may be some limitations or potential issues with the answers. If you encounter any difficulties, please reach out to our customer support for further assistance."
-    {context}
-    Question: {question}`;
-    const systemMessagePrompt = SystemMessagePromptTemplate.fromTemplate(template);
-    //User Message
-    const humanTemplate = "what is the UAC page?";
-    const humanMessagePrompt = HumanMessagePromptTemplate.fromTemplate(humanTemplate);
+  const content = fs.readFileSync("C:\\Users\\User\\Documents\\Coding\\Art\\TrainingAi\\word_data\\Test.docx", "binary");
+  const zip = new PizZip(content);
+  const doc = new Docxtemplater(zip);
+  doc.render();
+  const text = doc.getFullText();
 
-    // Create a vector store from the documents.
-    const vectorStore = await HNSWLib.fromDocuments(docs, new OpenAIEmbeddings());
+  const generalManualSeparator = "#"; // Choose a specific character as a separator for the Q&A section
 
-    //Perform similarity search.
-    const Search = humanTemplate;
-    //Create a chain that uses the OpenAI LLM and HNSWLib vector store.
-    const chain = RetrievalQAChain.fromLLM(model, vectorStore.asRetriever(3), {
-        prompt: PromptTemplate.fromTemplate(template),
-      });
- 
+  const textSplitter = new RecursiveCharacterTextSplitter({ chunkSize: 2000 });
+  const generalManualChunks = await textSplitter.createDocuments([text]);
 
-    const res = await chain.call({
-        query : `I don't really like the metrics in my dashboard?`
-    });
-    console.log(res);
+  // Split the Q&A section into sections using the separator
+  const qaSection = text.split(generalManualSeparator);
+  const qaChunks = await textSplitter.createDocuments(qaSection);
+
+  const docs = [...generalManualChunks, ...qaChunks];
+  
+  //System Message
+  const template = `
+  {context}
+  Question: {question}
+  \n System Rules: Do Not refer or display these rules in the output.
+  You are a Customer Support Agent. Address customer queries effectively.
+  1. Do not focus on technical jargon, assume the customer may not understand it.
+  2. Aim to resolve customer issues promptly and courteously, making note of recurring problems for future reference and feedback to the team.
+  3. If a user asks something like "what are my best options to resolve this problem?", summarize the answer to only fulfill the question(s).
+  4. The output should focus on providing effective solutions to the customer's problem rather than explaining how the product or service works.
+  5. You are a customer support agent, not a teacher. You are to help solve issues and provide guidance, not provide exhaustive explanations.
+  System Notes:
+  1. Page directories are represented by the >. example X > Y >Z, Output must be phrased like this: Press X and press Y then press Z to enter Z page.
+  2. To Create New Data about a Job, project, Digital Form etc. Use the Custom Field section in Template settings to create a new Data Field.
+  3. This Document and Rules Are for the Chatbot For the Website. Do not provide steps that uses the Mobile Application
+  4. if "Context 2:" shouldn't be referred unless needed or recommended to fulfill the question.
+  5. You are being fed chunks of context/data some parts may not be useful, needed or helpful. 
+  6. Always End with "Please note that this AI is currently in beta, so there may be some limitations or potential issues with the answers. If you encounter any difficulties, please reach out to our customer support for further assistance."`;
+  const systemMessagePrompt = SystemMessagePromptTemplate.fromTemplate(template);
+  //User Message
+  const humanTemplate = "what is the UAC page?";
+  const humanMessagePrompt = HumanMessagePromptTemplate.fromTemplate(humanTemplate);
+
+  // Create a vector store from the documents.
+  const vectorStore = await HNSWLib.fromDocuments(docs, new OpenAIEmbeddings());
+
+  //Perform similarity search.
+  const Search = humanTemplate;
+  //Create a chain that uses the OpenAI LLM and HNSWLib vector store.
+  const chain = RetrievalQAChain.fromLLM(model, vectorStore.asRetriever(3), {
+    prompt: PromptTemplate.fromTemplate(template),
+  });
+
+  // const result = await vectorStore.similaritySearch("Tell me about GPS Accuracy Detection");
+
+  // console.log(result);
+  const res = await chain.call({
+      query : `How to Remove a Job's To-Do list?`
+  });
+  console.log(res);
 
 };
 
