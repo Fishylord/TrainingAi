be765ae796f62b28d6c24d020cfc83bf1fc56302
Fishylord <chunyee04@outlook.com>
2023-08-09 08:43:40 +0800

    A

--- Filter.js
+++ Filter.js
@@ -42,17 +42,21 @@ export const run = async () => {
   Question: {question}
   You are to create "Json Filter Queries" From Questions created by Clients/Customers. 
   System Rules:
-  1. Output should not need \n or spaced out using + just one string
-  2. Do not Answer With any text Other than The Output Expected. 
-  3. Start and End with Curly Brackets, 1st Filter will be "dynamicfields" then continued by other filters.
-  4. DynamicFields such as custom fields will be inside the Dynamic fields brackets.
-  5. DynamicField variables should use their variable ID's instead of their name/text
-  6. If a DynamicField is referenced it their question/Prompt And the dynamic ID is found in the context Include it in the output
-  Normal Search Operators 0: Contain,1: Not Contain,2: Equal,3:Not Equal.
-  Date Search Operators 0: Within,1:Not Within,2:More and Equals Than,3: Less and Equals Than,4: More than, 5: Less Than.
-  Output Examples: <"dynamicfields": <>,"assetcategory":<"operator":"0","value":123417>> (the < and > represent curly brackets) (If it is a Dynamic Field put the IDs as the filter search variable example "XXX":<operator>)
-  Date/Time Filter Sample: "activityenddate":<o"operator":"0","value":<"type":"Date","min":"2023-08-09","max":"2023-08-17">>
-  Number Contain/Non Contain Operator: "dealseqno":<"operator":"0","value":<"type":"Number","min":0,"max":"100">> (Only For Values, if type is text min and max would just be "")`;
+  1. Output must be a single continuous string without any newline (\n) or added spaces.
+  2. The only response should be the desired output.
+  3. Begin and end the output with curly brackets. Start with the dynamicfields filter followed by other filters.
+  4. When including dynamicfields (custom fields), they should be nested within the dynamicfields brackets.
+  5. Use the variable ID for dynamicfields, not their descriptive name or text. 
+  6. If a dynamicfield is mentioned in the client's question and its dynamic ID is present in the context, include it in the output.
+  7. For dynamicfields, use this format: "dynamicfields" :<"DynamicField ID":<"operator":X,"value":<"value":"ABC","type":"Type","min":"","max":"">>>. Replace ABC with the actual value being searched.
+  8. if a Sample is provided you must use the sample Filter Structure provided.
+  Operator Guide:
+  1. Number and Text Type Filter Operators 0: Contain,1: Not Contain,2: Equal (Only takes 1 Number No minmax),3:Not Equal.
+  2. Date Filter Operators 0: Within,1:Not Within,2:More and Equals Than,3: Less and Equals Than,4: More than, 5: Less Than. 
+  Sample Outputs:
+  1. Output Examples: <"dynamicfields": <>,"assetcategory":<"operator":"0","value":123417>> (the < and > represent curly brackets) (If it is a Dynamic Field put the IDs as the filter search variable example "XXX":<operator>)
+  2. Date/Time Filter Sample: "activityenddate":<"operator":"0","value":<"type":"Date","min":"2023-08-09","max":"2023-08-17">>
+  3. Number Contain/Non Contain Operator: "dealseqno":<"operator":"0","value":<"type":"Number","min":0,"max":"100">> (Only For Values, if type is text min and max would just be "")`;
   
  
   //User Message
@@ -64,14 +68,14 @@ export const run = async () => {
   const chain = RetrievalQAChain.fromLLM(model, vectorStore.asRetriever(), {
     prompt: PromptTemplate.fromTemplate(template),
   }); 
-  
+
 
   const res = await chain.call({
-      query : `SRE Info with the text Sales that were given more than 10% discounts that has around 50-100$ more in collections from activities created by John in a activity between August 1-4th` 
+      query : `SRE Info with the text Sales that were given more than 10% discounts (Can't use Date Search operators) that has around 50-100$ more in collections from activities created by John in a activity between August 1-4th` 
   });
   console.log(res);
 
 };
 
+
 run();
- 
