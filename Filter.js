33152aa515f1ad1dd081067d48913929fc6c214c
Fishylord <chunyee04@outlook.com>
2023-08-10 09:05:44 +0800

    A

--- Filter.js
+++ Filter.js
@@ -51,12 +51,13 @@ export const run = async () => {
   7. For dynamicfields, use this format: "dynamicfields" :<"DynamicField ID":<"operator":X,"value":<"value":"ABC","type":"Type","min":"","max":"">>>. Replace ABC with the actual value being searched.
   8. if a Sample is provided you must use the sample Filter Structure provided.
   Operator Guide:
-  1. Number and Text Type Filter Operators 0: Contain,1: Not Contain,2: Equal (Only takes 1 Number No minmax),3:Not Equal.
+  1. Number and Text Type Filter Operators 0: Contain (use this for discount),1: Not Contain,2: Equal (Only takes 1 Number No minmax),3:Not Equal.
   2. Date Filter Operators 0: Within,1:Not Within,2:More and Equals Than,3: Less and Equals Than,4: More than, 5: Less Than. 
   Sample Outputs:
   1. Output Examples: <"dynamicfields": <>,"assetcategory":<"operator":"0","value":123417>> (the < and > represent curly brackets) (If it is a Dynamic Field put the IDs as the filter search variable example "XXX":<operator>)
   2. Date/Time Filter Sample: "activityenddate":<"operator":"0","value":<"type":"Date","min":"2023-08-09","max":"2023-08-17">>
-  3. Number Contain/Non Contain Operator: "dealseqno":<"operator":"0","value":<"type":"Number","min":0,"max":"100">> (Only For Values, if type is text min and max would just be "")`;
+  3. Number Contain/Non Contain Operator: "dealseqno":<"operator":"0","value":<"type":"Number","min":0,"max":"100">> (Only For Values, if type is text min and max would just be "")
+  Output must be a single continuous string without any newline (\n) or added spaces.`;
   
  
   //User Message
@@ -65,16 +66,16 @@ export const run = async () => {
 
 
   //Create a chain that uses the OpenAI LLM and HNSWLib vector store.
-  const chain = RetrievalQAChain.fromLLM(model, vectorStore.asRetriever(), {
+  const chain = RetrievalQAChain.fromLLM(model, vectorStore.asRetriever(10), {
     prompt: PromptTemplate.fromTemplate(template),
   }); 
 
 
   const res = await chain.call({
-      query : `SRE Info with the text Sales that were given more than 10% discounts (Can't use Date Search operators) that has around 50-100$ more in collections from activities created by John in a activity between August 1-4th` 
+      query : `SRE Info with the text Sales that were given 10% or more discounts (Can't use Date Search operators) that has around 50-100$ more in collections from activities created by John in a activity between August 1-4th` 
   });
   console.log(res);
-
+  
 };
 
 
