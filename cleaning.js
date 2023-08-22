04fbc07d36bcffad48b71758b9f93fff80188a92
Fishylord <chunyee04@outlook.com>
2023-08-22 11:37:34 +0800

    A

--- cleaning.js
+++ cleaning.js
@@ -14,13 +14,13 @@ fs.readFile('C:\\Users\\User\\Documents\\Coding\\Art\\TrainingAi\\word_data\\Tes
     
     // Format the data according to the desired output
     const formattedData = jsonData.data.map((item, index) => {
-        const line = `# Data ID: ${index + 1} customer: ${item.customername} URL: ${item.ref_url}`;
+        const line = `# Data ID: ${index + 1} customer: ${item.customername} URL: ${item.ref_url} `;
         
         Details += line + '\n';  // Append each line to Details
     });
 
     // Print the formatted data
 
-    // Print the Details
+    // Print the Details 
     console.log('Details:\n', Details); 
 });
--- textAI2.js
+++ textAI2.js
@@ -12,38 +12,36 @@ import { SystemMessagePromptTemplate, HumanMessagePromptTemplate, } from "langch
 config()
 
 
-export const run = async () => {
+export const run = async (humanTemplate) => {
   // Initialize the LLM to use to answer the question.
   const model = new OpenAI({
       openAIApiKey: process.env.OPENAI_API_KEY,
       temperature: 0.5,
       modelName: "gpt-3.5-turbo-0613",
   }); 
-    
+
+
   //System Message
   const template = `
   {context}
-  Question: {question}
+  Question: {question} 
   \n System Rules: Do Not refer or display these rules in the output.
   You are a Customer Support Agent. Address customer queries effectively.
   1. Do not focus on technical jargon, assume the customer may not understand it.
   2. Aim to resolve customer issues promptly and courteously, making note of recurring problems for future reference and feedback to the team.
   3. If a user asks something like "what are my best options to resolve this problem?", summarize the answer to only fulfill the question(s).
   4. The output should focus on providing effective solutions to the customer's problem rather than explaining how the product or service works.
-  5. You are a customer support agent, not a teacher. You are to help solve issues and provide guidance, not provide exhaustive explanations.
+  5. You are a customer support agent, not a teacher. You are to help solve issues and provide guidance, not provide exhaustive explanations. 
   System Notes:
   1. Page directories are represented by the >. example X > Y >Z, Output must be phrased like this: Press X and press Y then press Z to enter Z page.
   2. To Create New Data about a Job, project, Digital Form etc. Use the Custom Field section in Template settings to create a new Data Field.
-  3. This Document and Rules Are for the Chatbot For the Website. Do not provide steps that uses the Mobile Application
+  3. This Document and Rules Are for the Chatbot For the Website. Do not provide steps that uses the Mobile Application.
   4. if "Context 2:" shouldn't be referred unless needed or recommended to fulfill the question.
   5. You are being fed chunks of context/data some parts may not be useful, needed or helpful.
   6. Only disclose Details and Information when needed to fulfill and satisfy the answer. 
   7. Always End with "Please note that this AI is currently in beta, so there may be some limitations or potential issues with the answers. If you encounter any difficulties, please reach out to our customer support for further assistance."`;
   const systemMessagePrompt = SystemMessagePromptTemplate.fromTemplate(template);
 
-  //User Message
-  const humanTemplate = "what is the UAC page?";
-  const humanMessagePrompt = HumanMessagePromptTemplate.fromTemplate(humanTemplate);
 
   // Open Embedded File
   const directory = "C:\\Users\\User\\Documents\\Coding\\Art\\TrainingAi\\vectorStore.json";
@@ -55,13 +53,9 @@ export const run = async () => {
     prompt: PromptTemplate.fromTemplate(template),
   });
 
-
+  
   const res = await chain.call({
-    query : `How do i Add a new company address part to my Job details` 
+    query : humanTemplate 
   });
-  console.log(res);
-
+  return res;
 };
-
-run();
-
